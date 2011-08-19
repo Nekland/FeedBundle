87cc0dc55a1add6004f6dc7f434b89303f878df3
yohang <yohan@giarelli.org>
2011-08-19 17:18:31 +0200

    Fixed some namespace issues and Added service.xml

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Nekland\RssBundle\DependencyInjection;
+namespace Nekland\FeedBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;

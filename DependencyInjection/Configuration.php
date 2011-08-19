740f1b46b545f2edfa30a37b9e1b7bd11ded5474
Nek <nek-@live.fr>
2011-08-19 17:13:17 +0200

    correction in the configuration file

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Nekland\RssBundle\DependencyInjection;
+namespace Nekland\FeedBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -13,10 +13,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('nekland_fil')
-                ->children()
-                    ->arrayNode('steps')->end()
-                ->end();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for

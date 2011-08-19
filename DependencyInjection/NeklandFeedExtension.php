38456c5d27e00b9b27ad5c8018f2375af7059ba4
Nek <nek-@live.fr>
2011-08-19 17:14:13 +0200

    Correction of the extension file in the dependency injection.

--- DependencyInjection/NeklandFeedExtension.php
+++ DependencyInjection/NeklandFeedExtension.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Nekland\RssBundle\DependencyInjection;
+namespace Nekland\FeedBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\FileLocator;
 
-class NeklandRssExtension extends Extension {
+class NeklandFeedExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
 //        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 //        $loader->load('services.yml');

b9bc85ca6193619ece6c54ecbe02a9802e3252fa
Nek <nek-@live.fr>
2011-08-19 18:44:16 +0200

    add Feed

---
+++ Feed.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Nekland\FeedBundle;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+class RssFeed {
+    protected $router;
+    
+    /**
+     * Config example:
+     * 'class' => 'Nekland\BlogBundle\Entity\Article' (who must implement FeedBundle\Item\RssItem)
+     * 'title' => 'My Rss title'
+     * 'description' => 'My Rss description'
+     * 'route' => 'My Rss site route' (home if not defined)
+     */
+    protected $config;
+    
+    /**
+     * @var string $filename
+     */
+    protected $filename;
+
+    public function __construct(Router $router, array $config) {
+        $this->router = $router;
+        $this->config = $config;
+        
+        $rc = new \ReflectionClass($this->config['class']);
+        if ($this->rc->hasMethod('getFilename')) {
+        
+            $name = $item->getFilename();
+        } else {
+        
+            $e = explode('\\', get_class($item));
+            $name = $e[count($e) - 1];
+        }
+        $this->new = file_exists($this->filename = __DIR__ . '/../Resources/public/rss/' . $name . 'Rss.xml') ? false : true;
+    }
+    
+    public function add($item){
+        $rc = new \ReflectionClass($this->config['class']);
+        if(!$rc->isInstance($item))
+            throw new \InvalidArgumentException();
+        
+        $rc = new \ReflectionClass($item);
+        if ($this->rc->hasMethod('getFilename')) {
+            $name = $item->getFilename();
+        } else {
+            $e = explode('\\', get_class($item));
+            $name = $e[count($e) - 1];
+        }
+        $this->new = file_exists($this->filename = __DIR__ . '/../Resources/public/rss/' . $name . 'Rss.xml') ? false : true;
+    }
+}

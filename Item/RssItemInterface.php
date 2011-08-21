eeac1b9296f1a7df3b2a267c10df6f289567e0d4
Nek <nek-@live.fr>
2011-08-21 23:11:20 +0200

    Correct comments on RssItemInterface (badtrip with doctrine, in fact a real wtf error)

--- Item/RssItemInterface.php
+++ Item/RssItemInterface.php
@@ -9,19 +9,19 @@ interface RssItemInterface {
      */
     public function getRssTitle();
     
-    /**
+    /*
      * Return the description of your rss, someting like "This is the rss of my blog about foo and bar"
      * @return string
      */
     public function getRssDescription();
     
-    /**
+    /*
      * Return the route of your item
      * @return string|array with [0] => 'route_name', [1] => params
      */
     public function getRssRoute();
     
-    /**
+    /*
      * @return unique identifiant (for editing)
      */
     public function getRssId();
